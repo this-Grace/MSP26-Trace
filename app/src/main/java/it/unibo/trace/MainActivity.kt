46b6d61e5537fef65615179b153ffff880e360a4
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2026-04-27 22:35:36 +0200

    feat: implement Jetpack Navigation and setup Home route

--- app/src/main/java/it/unibo/trace/MainActivity.kt
+++ app/src/main/java/it/unibo/trace/MainActivity.kt
@@ -3,14 +3,10 @@ package it.unibo.trace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
-import it.unibo.trace.ui.screen.HomeScreen
+import it.unibo.trace.ui.NavGraph
 import it.unibo.trace.ui.theme.TraceTheme
 
 val supabase = createSupabaseClient(
@@ -24,8 +20,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TraceTheme() {
+            TraceTheme {
                 val navController = rememberNavController()
+                NavGraph(navController = navController)
             }
         }
     }
---
+++ app/src/main/java/it/unibo/trace/ui/Navigation.kt
@@ -0,0 +1,25 @@
+package it.unibo.trace.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import it.unibo.trace.ui.screen.HomeScreen
+import kotlinx.serialization.Serializable
+
+sealed interface Route {
+    @Serializable data object Home : Route
+
+}
+
+@Composable
+fun NavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Route.Home
+    ) {
+        composable<Route.Home> {
+            HomeScreen(navController)
+        }
+    }
+}
