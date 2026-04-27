6cc14255c0814289e8d38c43e9195455a7ec59d2
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2026-04-27 22:05:39 +0200

    feat: add Jetpack Navigation Compose dependency

--- app/src/main/java/it/unibo/trace/MainActivity.kt
+++ app/src/main/java/it/unibo/trace/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
 import it.unibo.trace.ui.screen.HomeScreen
+import it.unibo.trace.ui.theme.TraceTheme
 
 val supabase = createSupabaseClient(
     supabaseUrl = BuildConfig.SUPABASE_URL,
@@ -22,13 +24,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    HomeScreen()
-                }
+            TraceTheme() {
+                val navController = rememberNavController()
             }
         }
     }
