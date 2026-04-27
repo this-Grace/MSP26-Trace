f513c23faff5c741784043d224d5ab24b3e56679
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2026-04-27 23:21:14 +0200

    style: implement Space Grotesk as the global typeface

--- app/src/main/java/it/unibo/trace/ui/theme/Type.kt
+++ app/src/main/java/it/unibo/trace/ui/theme/Type.kt
@@ -1,34 +1,31 @@
 package it.unibo.trace.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import it.unibo.trace.R.font
 
-// Set of Material typography styles to start with
+val SpaceGrotesk = FontFamily(
+    Font(font.space_grotesk, FontWeight.Medium)
+)
+
+private val defaultTypography = Typography()
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    displayLarge = defaultTypography.displayLarge.copy(fontFamily = SpaceGrotesk),
+    displayMedium = defaultTypography.displayMedium.copy(fontFamily = SpaceGrotesk),
+    displaySmall = defaultTypography.displaySmall.copy(fontFamily = SpaceGrotesk),
+    headlineLarge = defaultTypography.headlineLarge.copy(fontFamily = SpaceGrotesk),
+    headlineMedium = defaultTypography.headlineMedium.copy(fontFamily = SpaceGrotesk),
+    headlineSmall = defaultTypography.headlineSmall.copy(fontFamily = SpaceGrotesk),
+    titleLarge = defaultTypography.titleLarge.copy(fontFamily = SpaceGrotesk),
+    titleMedium = defaultTypography.titleMedium.copy(fontFamily = SpaceGrotesk),
+    titleSmall = defaultTypography.titleSmall.copy(fontFamily = SpaceGrotesk),
+    bodyLarge = defaultTypography.bodyLarge.copy(fontFamily = SpaceGrotesk),
+    bodyMedium = defaultTypography.bodyMedium.copy(fontFamily = SpaceGrotesk),
+    bodySmall = defaultTypography.bodySmall.copy(fontFamily = SpaceGrotesk),
+    labelLarge = defaultTypography.labelLarge.copy(fontFamily = SpaceGrotesk),
+    labelMedium = defaultTypography.labelMedium.copy(fontFamily = SpaceGrotesk),
+    labelSmall = defaultTypography.labelSmall.copy(fontFamily = SpaceGrotesk)
 )
