f5654b0a22a8db99e803bfcec20c8bcd9d12dd07
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2026-04-27 23:22:48 +0200

    style: update theme to custom Cyan palette and improve UI consistency

--- app/src/main/java/it/unibo/trace/ui/theme/Theme.kt
+++ app/src/main/java/it/unibo/trace/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package it.unibo.trace.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,34 +11,35 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = cyan,
+    onPrimary = black,
+    primaryContainer = cyan,
+    onPrimaryContainer = black,
+    surface = black,
+    onSurface = white,
+    background = black,
+    onBackground = white,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = cyan,
+    onPrimary = black,
+    primaryContainer = cyan,
+    onPrimaryContainer = black,
+    surface = white,
+    onSurface = black,
+    background = white,
+    onBackground = black,
 )
 
 @Composable
 fun TraceTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
+
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
